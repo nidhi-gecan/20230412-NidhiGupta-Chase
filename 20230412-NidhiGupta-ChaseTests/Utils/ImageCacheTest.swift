240ed2b2c8d8eaa6c6d343ef9d6bf64aac248f4e
Nidhi Gupta <nidhi.gupta@globant.com>
2023-04-14 11:51:02 +0530

    Updated Unit Test case for ImageCache

--- 20230412-NidhiGupta-ChaseTests/Utils/ImageCacheTest.swift
+++ 20230412-NidhiGupta-ChaseTests/Utils/ImageCacheTest.swift
@@ -4,4 +4,12 @@ import XCTest
 
 class ImageCacheTest: XCTestCase {
 
+    func testDownloadImage() {
+        let imageCache = ImageCache.shared
+        let url = URL(string: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")!
+        imageCache.downloadImage(url: url, icon: "google") { (image) in
+            XCTAssertNotNil(image)
+        }
+    }
+
 }

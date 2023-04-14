422915f5f7f28a1544761bf0bd068c0f5755f28b
Nidhi Gupta <nidhi.gupta@globant.com>
2023-04-14 12:02:26 +0530

    Fixed test cases

--- 20230412-NidhiGupta-ChaseTests/Network/EndpointsTest.swift
+++ 20230412-NidhiGupta-ChaseTests/Network/EndpointsTest.swift
@@ -7,19 +7,19 @@ class EndpointsTest: XCTestCase {
     func testWeatherByCity() {
         let city = "London"
         let url = Endpoints.weatherByCity(city: city).url
-        XCTAssertEqual(url?.absoluteString, "https://api.openweathermap.org/data/2.5/weather?q=London&appid=2f8b4c7b0b1b4c7b0b1b4c7b0b1b4c7b=metric")
+        XCTAssertEqual(url?.absoluteString, "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b418898fdb7e1d3f254ece4b2f2e8506&units=metric")
     }
     
     func testUrlForIconImage() {
         let icon = "01d"
         let url = Endpoints.urlForIconImage(icon: icon).url
-        XCTAssertEqual(url?.absoluteString, "https://openweathermap.org/img/wn/01d.png")
+        XCTAssertEqual(url?.absoluteString, "https://openweathermap.org/img/wn/01d@2x.png")
     }
     
     func testFetchCityFromLocation() {
         let lat = 51.5074
         let lon = 0.1278
         let url = Endpoints.fetchCityFromLocation(lat: lat, lon: lon).url
-        XCTAssertEqual(url?.absoluteString, "https://api.openweathermap.org/geo/1.0/reverse?lat=51.5074&lon=0.1278&limit=1&appid=2f8b4c7b0b1b4c7b0b1b4c7b0b1b4c7b")
+        XCTAssertEqual(url?.absoluteString, "https://api.openweathermap.org/geo/1.0/reverse?lat=51.5074&lon=0.1278&limit=1&appid=b418898fdb7e1d3f254ece4b2f2e8506")
     }
 }
